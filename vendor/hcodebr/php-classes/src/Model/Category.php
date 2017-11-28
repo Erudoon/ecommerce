7c13b0f1253506dfbccf43bf63e9dc38f5db1841
Gabriel <gabriel.souza2@hotmail.com.br>
2017-11-28 21:03:54 -0200

    Admin - CRUD categorias

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -47,25 +47,4 @@ class Category extends Model{
             "idcategory"=>$this->getidcategory()
         ));
     }
-
-    /*
-
-    public function update()
-    {
-        $sql = new Sql();
-
-        $results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)",
-        array(
-            ":iduser"=>$this->getiduser(),
-            ":desperson"=>$this->getdesperson(),
-            ":deslogin"=>$this->getdeslogin(),
-            ":despassword"=>$this->getdespassword(),
-            ":desemail"=>$this->getdesemail(),
-            ":nrphone"=>$this->getnrphone(),
-            ":inadmin"=>$this->getinadmin()
-        ));
-
-        $this->setData($results[0]);
-    }
-    */
 }
